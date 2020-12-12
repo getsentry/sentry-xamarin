c4addc9f3b00434dbebbe9a937a48a4f528ac570
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2020-12-12 10:24:47 -0300

    Add breadcrumbs to ios.

--- Sentry.Xamarin.Forms/Internals/NativeIntegration.ios.cs
+++ Sentry.Xamarin.Forms/Internals/NativeIntegration.ios.cs
@@ -1,11 +1,56 @@
-﻿using Sentry.Integrations;
+﻿using Foundation;
+using Sentry.Integrations;
+using Sentry.Protocol;
+using System;
+using System.Collections.Generic;
+using UIKit;
 
 namespace Sentry.Xamarin.Forms.Internals
 {
     internal partial class NativeIntegration : ISdkIntegration
     {
         internal bool Implemented => true;
-        public void Register(IHub hub, SentryOptions options) { }
-        internal void Unregister() { }
+        private List<NSObject> _observerTokens;
+        public void Register(IHub hub, SentryOptions options)
+        {
+            _observerTokens = new List<NSObject>();
+            _observerTokens.Add(NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.DidEnterBackgroundNotification, AppEnteredBackground));
+            _observerTokens.Add(NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.WillEnterForegroundNotification, AppEnteredForeground));
+            _observerTokens.Add(NSNotificationCenter.DefaultCenter.AddObserver(UIApplication.DidReceiveMemoryWarningNotification, MemoryWarning));
+        }
+        internal void Unregister()
+        {
+            NSNotificationCenter.DefaultCenter.RemoveObservers(_observerTokens);
+        }
+
+        internal Action<NSNotification> AppEnteredBackground = (_) =>
+        {
+            SentrySdk.AddBreadcrumb(null,
+                "ui.lifecycle",
+                "navigation", data: new Dictionary<string, string>
+                {
+                    ["screen"] = SentryXamarinFormsIntegration.CurrentPage,
+                    ["state"] = "background"
+                }, level: BreadcrumbLevel.Info);
+        };
+
+        internal Action<NSNotification> AppEnteredForeground = (_) =>
+        {
+            SentrySdk.AddBreadcrumb(null,
+                "ui.lifecycle",
+                "navigation", data: new Dictionary<string, string>
+                {
+                    ["screen"] = SentryXamarinFormsIntegration.CurrentPage,
+                    ["state"] = "foreground"
+                }, level: BreadcrumbLevel.Info);
+        };
+
+        internal Action<NSNotification> MemoryWarning = (_) =>
+        {
+            SentrySdk.AddBreadcrumb("low memory",
+                "xamarin",
+                "info",
+                level: BreadcrumbLevel.Warning);
+        };
     }
 }
