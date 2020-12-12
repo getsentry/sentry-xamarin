37802a99a996e55768520b322ead82b744911c67
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2020-12-11 22:15:44 -0300

    fix typo

--- Sentry.Xamarin.Forms/Extensions/SentryOptionsExtensions.cs
+++ Sentry.Xamarin.Forms/Extensions/SentryOptionsExtensions.cs
@@ -5,7 +5,12 @@ namespace Sentry
 {
     public static class SentryOptionsExtensions
     {
-        public static void DisableBreadcrumbForXamlWarnings(SentryOptions option)
+        public static void DisableBreadcrumbForXamlWarnings(this SentryOptions option)
+        {
+            SentryXamarinFormsIntegration.LogXamlErrors = false;
+        }
+
+        public static void DisableNativeIntegration(this SentryOptions options)
         {
             SentryXamarinFormsIntegration.Instance.UnregisterNativeIntegration();
         }
