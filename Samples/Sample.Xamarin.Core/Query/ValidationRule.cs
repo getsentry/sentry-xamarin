54b4479d3c5979e0b258f8c101102aa4bc73a67b
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2020-12-16 17:06:09 -0300

    removed unused files.

--- Samples/Sample.Xamarin.Core/Query/ValidationRule.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sample.Xamarin.Core.Query
-{
-    public class ValidationRule
-    {
-        public string ValidationMessage { get; set; }
-        public bool Check { get; set; }
-    }
-}
