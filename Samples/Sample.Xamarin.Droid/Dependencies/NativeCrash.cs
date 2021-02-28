8c1c0e56cbb1e903d4dce3d09aa4b6c095aceb33
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-02-28 17:41:20 -0300

    Added Native Library sample crash for Android.

---
+++ Samples/Sample.Xamarin.Droid/Dependencies/NativeCrash.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Sentry.Ovenlibrary;
+using Sample.Xamarin.Core.Interfaces;
+using Sample.Xamarin.Droid.Dependencies;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(NativeCrash))]
+namespace Sample.Xamarin.Droid.Dependencies
+{
+    public class NativeCrash : INativeCrash
+    {
+        public void BrokenNativeCallback()
+        {
+            var oven = new Oven();
+            oven.Cook();
+        }
+    }
+}
