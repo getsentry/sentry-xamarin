3e9df6448ecfd901d5da31bb0ae7240b5bcdcf2e
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2022-04-11 15:56:11 -0300

    fix 0 byte screenshot being sent.

--- Src/Sentry.Xamarin/Internals/Device/Screenshot/ScreenshotAttachmentContent.cs
+++ Src/Sentry.Xamarin/Internals/Device/Screenshot/ScreenshotAttachmentContent.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using Sentry.Extensibility;
+﻿using System.IO;
 using Xamarin.Essentials;
 using static Xamarin.Essentials.Screenshot;
 
@@ -8,21 +6,10 @@ namespace Sentry.Internals.Device.Screenshot
 {
     internal class ScreenshotAttachmentContent : IAttachmentContent
     {
-        private SentryXamarinOptions _options { get; }
-        public ScreenshotAttachmentContent(SentryXamarinOptions options) => _options = options;
-
         public Stream GetStream()
         {
-            try
-            {
                 var screenStream = CaptureAsync().ConfigureAwait(false).GetAwaiter().GetResult();
                 return screenStream.OpenReadAsync(ScreenshotFormat.Jpeg).ConfigureAwait(false).GetAwaiter().GetResult();
-            }
-            catch (Exception ex)
-            {
-                _options.DiagnosticLogger?.LogError("Failed to capture a screenshot.", ex);
-            }
-            return new MemoryStream();
         }
     }
 }
